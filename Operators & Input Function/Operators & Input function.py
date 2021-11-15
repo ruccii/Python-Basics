35f5269fbab9f8a12f340fee83c727b4de1a9336
ruccii <30281464+ruccii@users.noreply.github.com>
2021-11-16 02:57:27 +0530

    Added a program file

---
+++ Operators & Input Function/Operators & Input function.py
@@ -0,0 +1,55 @@
+a = "Ruchi"
+b = 365
+c = 3.5
+d = False
+
+#printing variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+#checking data type
+print(type(a))
+print(type(c))
+print(type(d))
+
+#operators
+A = 12
+B = 45
+
+#arithmetic operators
+print('Value of 12+45 is=', 12+45)
+
+# Assignment operators
+
+a = 45
+a += 20
+a -= 15
+a *= 2
+a /= 2
+
+print(a)
+
+#comparison operators
+
+b = (14>7)
+print(b)
+
+b = (14<=7)
+print(b)
+
+b = (14!=7)
+print(b)
+
+#input function
+
+a = input("Enter your name: ")
+print(type(a))
+
+a = input("Enter your name: ")
+a = int(a)   #this is called typecasting, to convert into an integer, if possible only
+print(type(a))
+
+a = input("Enter your name: ")
+print(type(a))
