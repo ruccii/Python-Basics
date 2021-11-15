2f15a3dbbbafa5f4c014bac9b060eef240359b22
ruccii <30281464+ruccii@users.noreply.github.com>
2021-11-16 03:08:23 +0530

    Added a new file
    Calculating average of two numbers given by the users.

---
+++ Calculating average with user inputs/Average.py
@@ -0,0 +1,8 @@
+# average of two numbers given by the users
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+a = int(a)
+b = int(b)
+avg = (a + b)/2
+print("The average of two numbers is: ", avg)
