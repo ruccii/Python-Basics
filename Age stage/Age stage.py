5fbc4c1e50dfa6ebb63a7f91fe1ea3285fae7853
ruccii <30281464+ruccii@users.noreply.github.com>
2021-11-21 13:39:21 +0530

    Program with output

---
+++ Age stage/Age stage.py
@@ -0,0 +1,8 @@
+#check if a person is an adult or not
+name = (input("Enter your name: "))
+age = int(input("Enter your age: "))
+if(age>18):
+    print("You are an adult")
+else:
+    (age<18)
+    print("you are a child")
