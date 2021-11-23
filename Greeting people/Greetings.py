62301f199f680caa96ed5eba20b4e00e0499165b
ruccii <30281464+ruccii@users.noreply.github.com>
2021-11-23 16:35:38 +0530

    Python basics

---
+++ Greeting people/Greetings.py
@@ -0,0 +1,12 @@
+# name = input("Enter your name: \n")
+# print("Good Afternoon, "   +name)
+
+
+def greet(name):
+    return "Good Day " + name
+    
+
+print(greet("Tom"))
+print(greet("Jerry"))
+print(greet("Tweety"))
+print(greet("Bunny"))
