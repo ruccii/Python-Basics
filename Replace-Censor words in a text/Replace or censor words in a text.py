717dc508cd38942d3856264de39917820fd194c1
ruccii <30281464+ruccii@users.noreply.github.com>
2021-11-23 16:27:19 +0530

    Python Basics

---
+++ Replace-Censor words in a text/Replace or censor words in a text.py
@@ -0,0 +1,21 @@
+# with open("sample.txt") as a:           #read file
+#     content = a.read()
+
+# content = content.replace('donkey', '#%$^%&')  #perform task
+
+# with open(f"sample.txt", "w") as a:
+#     a.write(content)
+
+#MULTIPLE WORDS REPLACE
+
+words = ["donkey", "is", "animal"]
+with open("sample.txt") as a: #read file
+     content = a.read()
+
+for word in words:
+    content = content.replace(word, '#%$^%&')  #perform task
+
+with open(f"sample.txt", "w") as a:
+     a.write(content)
+
+print("done")
